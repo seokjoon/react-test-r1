44c300646b1552156e8a361381fb8fd19918bca3
seokjoon <seokjoon@gmail.com>
2018-09-25 10:02:42 +0900

    yarn eject

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 //import App from './App';
 //import Ch2 from './Ch2/Ch2';
 //import Ch3 from './Ch3/Ch3';
 //import Ch4 from './Ch4/Ch4';
 //import Ch5 from './Ch5/Ch5';
 import Ch6 from "./Ch6/Ch6";
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Ch6/>, document.getElementById('root'))
 //ReactDOM.render(<Ch5/>, document.getElementById('root'));
