711e9b315bda4ac93670f826b9707b5a250666e9
seokjoon <seokjoon@gmail.com>
2018-09-25 10:39:53 +0900

    리액트를 다루는 기술.ch9: 컴포넌트 스타일링: Sass, 변수, 믹스인

--- src/Ch9/Ch9.js
+++ src/Ch9/Ch9.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 //import classNames from 'classnames';
 import classNames from 'classnames/bind';
-import styles from './Ch9.css';
+//import styles from './Ch9.css';
+import styles from './Ch9.scss';
 
 const cx = classNames.bind(styles);
 
@@ -11,9 +12,14 @@ class Ch9 extends Component {
 		return (
 			//<div className={[styles.box, styles.blue].join(' ')}>
 			//<div className={classNames(styles.box, styles.blue)}>
-			<div className={cx('box', {blue:isBlue})}>
+			//<div className={cx('box', {blue:isBlue})}>
+			//</div>
+			<div className={cx('box', { blue:isBlue })}>
+				<div className={cx('box-inside')}>
+				</div>
 			</div>
 		);
+
 	}
 }
 
